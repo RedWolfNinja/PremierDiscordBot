77d3b2e12b3b0b6b3fa1a50addc26d95fb3368c2
Ēriks Boka <e@boka.lv>
2025-10-27 00:23:45 +0200

    slash commands implementation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+from discord import app_commands
 import os
 from schedule import *
 from datetime import date
@@ -9,18 +10,28 @@ intents.message_content = True
 api_key = os.getenv('PREMIER_BOT_API_KEY')
 
 client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
 
 @client.event
 async def on_ready():
     print(f"Logged in as {client.user}")
+    try:
+        await tree.sync()
+    except Exception as e:
+        print(e)
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+# @client.event
+# async def on_message(message):
+#     if message.author == client.user:
+#         return
 
-    if message.content.startswith('/gametoday'):
-        await message.channel.send(f"**{gameType[date.today().weekday()]}** today at: **{premierSchedule[date.today().weekday()]}**")
+#     if message.content.startswith('/gametoday'):
+#         await message.channel.send(f"**{gameType[date.today().weekday()]}** today at: **{premierSchedule[date.today().weekday()]}**")
+
+
+@tree.command(name="gametoday", description="Returns today's game schedule and type (Scrim or Match)")
+async def gametoday(interaction: discord.Integration):
+    await interaction.response.send_message(f"**{gameType[date.today().weekday()]}** today at: **{premierSchedule[date.today().weekday()]}**")
 
 client.run(api_key)
 
